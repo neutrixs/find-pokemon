590e9d932ad32b969b1dce74e021cdec83182b02
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2022-06-26 12:25:04 +0700

    add filter element

--- src/filter/filter.tsx
+++ src/filter/filter.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useContext, useRef } from 'react'
+import { PokemonDataContext } from '../store'
 import style from './filterStyle.module.scss'
 
 export default function Filter() {
+    const { setFilter } = useContext(PokemonDataContext)
+    const inputElement = useRef<HTMLInputElement>(null)
+    const setFilterTimeout = useRef<NodeJS.Timeout>(setTimeout(() => {}))
+
+    function onChange() {
+        clearTimeout(setFilterTimeout.current)
+        setFilterTimeout.current = setTimeout(() => {
+            setFilter(inputElement.current?.value || '')
+        }, 200)
+    }
+
     return (
         <div className={style.inputHolder}>
-            <input className={style.input} placeholder="Filter pokémon by its name" />
+            <input className={style.input} placeholder="Filter pokémon by its name" onChange={onChange} ref={inputElement} />
         </div>
     )
 }
