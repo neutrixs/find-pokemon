98642221ad1df282fdebb11cedcef9b0d1b5928f
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2022-06-26 11:49:51 +0700

    working addData function

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import { pokemonDataType, PokemonDataContext as DataContext } from './store'
 import './globalStyle.scss'
@@ -8,7 +8,55 @@ function Main() {
     const [pokemonData, setPokemonData] = useState<pokemonDataType[]>([])
     const nextURL = useRef('')
 
-    async function addData() {}
+    async function addData() {
+        const URL = nextURL.current || 'https://pokeapi.co/api/v2/pokemon?limit=50'
+
+        const rawPokemonLists = await fetch(URL)
+        const pokemonLists = await rawPokemonLists.json()
+
+        nextURL.current = pokemonLists.next
+
+        const promises: Array<Promise<void>> = []
+        const tempPokemonData: pokemonDataType[] = []
+
+        async function getAPokemonFullInfo(thisPokemon: any, i: number) {
+            const thisPokemonName = thisPokemon.name
+
+            const rawThisPokemonData = await fetch(thisPokemon.url)
+            const thisPokemonData = await rawThisPokemonData.json()
+
+            const rawThisAbilityInfo = await fetch(thisPokemonData.abilities[0].ability.url)
+            const thisAbilityInfo = await rawThisAbilityInfo.json()
+
+            const thisPokemonShortEffect = thisAbilityInfo.effect_entries.find(
+                (entry: any) => entry.language.name == 'en'
+            )?.short_effect
+
+            const thisPokemonFlavorTextEntries = thisAbilityInfo.flavor_text_entries.find(
+                (entry: any) => entry.language.name == 'en'
+            )?.flavor_text
+
+            tempPokemonData[i] = {
+                name: thisPokemonName,
+                short_effect: thisPokemonShortEffect,
+                flavor_text_entries: thisPokemonFlavorTextEntries,
+            }
+        }
+
+        pokemonLists.results.forEach((thisPokemon: any, i: number) => {
+            promises[i] = getAPokemonFullInfo(thisPokemon, i)
+        })
+
+        for (const promise of promises) {
+            await promise
+        }
+
+        setPokemonData(prev => prev.concat(...tempPokemonData))
+    }
+
+    useEffect(() => {
+        addData()
+    }, [])
 
     return (
         <>
